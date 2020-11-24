d0fed58d3ea20234b49ed88f6e24708e04725f8f
rosergeev <rosergeev@gmail.com>
2020-11-24 08:41:41 +0000

    Add project files.

---
+++ GetItemIdWeb/ItemId.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            var itemElem = document.getElementById("itemId");
+            var p = document.createElement("p");
+            p.innerHTML = Office.context.mailbox.item.itemId;
+            itemElem.appendChild(p);
+        });
+    };
+})();
