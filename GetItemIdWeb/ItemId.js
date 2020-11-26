651f8c72d36b81b6b54a82b6fe39e0dc622f7ff1
rosergeev <rosergeev@gmail.com>
2020-11-26 06:54:23 +0000

    Add conversion to ews format

--- GetItemIdWeb/ItemId.js
+++ GetItemIdWeb/ItemId.js
@@ -7,6 +7,11 @@
             var p = document.createElement("p");
             p.innerHTML = Office.context.mailbox.item.itemId;
             itemElem.appendChild(p);
+            var br = document.createElement("br");
+            itemElem.appendChild(br);
+            var p2 = document.createElement("p");
+            p2.innerHTML = "convertToRestId: <br>" + Office.context.mailbox.convertToEwsId(Office.context.mailbox.item.itemId, Office.MailboxEnums.RestVersion.v2_0);
+            itemElem.appendChild(p2);
         });
     };
 })();
